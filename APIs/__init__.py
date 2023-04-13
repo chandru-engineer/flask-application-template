c906298bb029b3ff1af47c0baaeb362c19ee34e3
Chandru <86843423+chandru-engineer@users.noreply.github.com>
2023-04-13 16:27:14 +0530

    Add files via upload

--- APIs/__init__.py
+++ APIs/__init__.py
@@ -7,16 +7,16 @@ from APIs.log_config import formatter, handler
 from flask_admin import Admin
 from flask_login import LoginManager
 from flask_admin.menu import MenuLink
-
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import AdminIndexView, expose
+from flask_login import current_user
 
 # creating class instance
 app = Flask(__name__, template_folder='templates')
 
-
 login_manager = LoginManager()
 db = SQLAlchemy()
 ma = Marshmallow()
-
 
 # setup the logging congiguration
 app.logger.addHandler(handler)
@@ -26,8 +26,19 @@ handler.setFormatter(formatter)
 # prevent the Flask logger from propagating log messages to the root logger
 app.logger.propagate = False
 
+def get_user_count():
+    from APIs.MMUser.model.user_model import User
+    users = User.query.filter_by(email=current_user.email).all()
+    return len(users)
+
+class MyAdminIndexView(AdminIndexView):
+    @expose('/')
+    def index(self):
+        user_count = get_user_count()
+        return self.render('admin/index.html', user_count=user_count, username=current_user.name)
+
 # configure admin panel
-admin = Admin(app, name='Control', template_mode='bootstrap3')
+admin = Admin(app, name='Control', index_view=MyAdminIndexView(), template_mode='bootstrap3')
 
 def create_app(config_class=Config):
     
@@ -58,6 +69,9 @@ def create_app(config_class=Config):
     # add models in admin panel 
     from APIs.MMUser.model.user_model import User, TableA, TableB, TableC, TableD
     from APIs.admin_view.schema.modelViewSchema import CustomModelView
+    
+    
+    # admin.add_link(MenuLink(name='Home Page', url='/', category='Links'))
     
     admin.add_view(CustomModelView(User, db.session))
     admin.add_view(CustomModelView(TableA, db.session))
